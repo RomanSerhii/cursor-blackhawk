cc49374909c7ecffdc9ccc27a07f76c9d35a2a2f
serhii <romansergiy2@gmail.com>
2020-05-25 23:52:02 +0300

    fix slider for responsive

--- HW9/js/script.js
+++ HW9/js/script.js
@@ -2,8 +2,19 @@ $(document).ready(function () {
   $(".slider").slick({
     arrows: true,
     dots: true,
+    adaptiveHeight: true,
     slidesToShow: 3,
     speed: 1000,
     centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        },
+      },
+    ],
   });
 });
