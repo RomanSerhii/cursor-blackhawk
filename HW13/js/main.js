abd5a9775c8bad5df9790af9d14a0ba72ccf9bcf
serhii <romansergiy2@gmail.com>
2020-06-14 22:38:05 +0300

    delete script.js

--- HW13/js/main.js
+++ HW13/js/main.js
@@ -2,3 +2,23 @@ $(window).on("load", () => {
   console.log("on load");
   $(".preloader-container").fadeOut(500);
 });
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    speed: 1000,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: true,
+        },
+      },
+    ],
+  });
+});
